2e9344b4192a3c27e732f6f68e07ebd1ea3f6db5
itsziel <azrilalle@gmail.com>
2023-06-04 08:56:39 +0700

    a

---
+++ Praktikum 6 1204210066/vite.config.js
@@ -0,0 +1,16 @@
+// Sebagai fitur refreshing on save
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
